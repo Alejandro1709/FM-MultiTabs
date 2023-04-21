7c34db2d5cb8c4b032344340ba31647a56d4be7f
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-04-21 13:04:07 -0500

    Add: Layout Structured blocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 function App() {
   return (
-    <>
-      <div>
-        <h1>App</h1>
+    <main className="flex md:justify-center md:items-center min-h-screen bg-background">
+      <div className="flex flex-col md:flex-row bg-white md:p-[16px] md:shadow-md md:h-[600px] flex-1 md:max-w-[940px] md:rounded-xl">
+        <aside className="md:w-[274px] h-[172px] md:h-full bg-lightBlue md:rounded-md">ASIDE</aside>
+        <section className="flex items-start flex-1 bg-pink">
+          <article className="max-w-[450px] mx-auto bg-white">
+            SECTION
+          </article>
+        </section>
       </div>
-    </>
+    </main>
   )
 }
 
