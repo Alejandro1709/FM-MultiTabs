6eec5114333337fa1f83ad4af97a9ec3f0b39fef
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-04-21 12:43:02 -0500

    Add: Colors and Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#EFF5FF',
+        border: '#D6D9E6',
+        denim: '#022959',
+        grey: '#9699AA',
+        lightBlue: '#ABBCFF',
+        lightGrey: '#D6D9E6',
+        orange: '#FFAF7E',
+        pink: '#F9818E',
+        purple: '#483EFF',
+        redErrors: '#EE374A',
+        skyBlue: '#BEE2FD',
+        veryLightGrey: '#F8F9FF',
+      },
+      fontFamily: {
+        ubuntu: ['Ubuntu', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
